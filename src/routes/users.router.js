53519587a729fa8b28e081e893a178bcdd7798e4
Richard Flores <lcdo.richardflores@gmail.com>
2024-12-16 21:10:51 -0300

    un par de cambios en el front

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -12,22 +12,21 @@ router.post("/login", userController.logIn);
 
 router.post("/logout", userController.logOut);
 
-////////////////////////
-////     Current      ////
-////////////////////////
+//Current
+
 router.get("/current", authMiddleware, (req, res) => {
-  try {
-    if (req.user.role == "admin") {
-      // si es admin entra a la parte de edicion
-      return res.redirect("/adminpage");
-    }
-    const userData = req.user;
-    // si es admin entra a la parte de edicion
-    // return res.redirect("/products");
-    return res.status(201).json({ message: userData });
-  } catch (error) {
-    res.status(500).json({ message: `error en ruta current ${error}` });
-  }
+	try {
+		if (req.user.role == "admin") {
+			// si es admin entra a la parte de edicion
+			return res.redirect("/adminpage");
+		}
+		const userData = req.user;
+		// si es admin entra a la parte de edicion
+		// return res.redirect("/products");
+		return res.status(201).json({ message: userData });
+	} catch (error) {
+		res.status(500).json({ message: `error en ruta current ${error}` });
+	}
 });
 
 export default router;
